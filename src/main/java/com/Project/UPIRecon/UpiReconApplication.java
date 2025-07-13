abd87188f3dc32ae7e08b70deeaba073d9b9a077
Afif-Shaikh <shaikhafif5@gmail.com>
2025-07-13 13:06:16 +0530

    1st commit

---
+++ src/main/java/com/Project/UPIRecon/UpiReconApplication.java
@@ -0,0 +1,13 @@
+package com.Project.UPIRecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiReconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiReconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Project/UPIRecon/UpiReconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project.UPIRecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpiReconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
